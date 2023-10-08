b9697d19af07d7957d3c56e00b513476ed189fad
JoelCoyos <coyos52@gmail.com>
2023-10-08 00:45:14 -0300

    eventos realidad

--- Assets/Scripts/RealityEvents/AmigoEvent.cs
+++ Assets/Scripts/RealityEvents/AmigoEvent.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AmigoEvent : MonoBehaviour
+public class AmigoEvent : RealityEvent
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Collider2D amigoCollider;
+
+    private bool isAmigoHappy;
+
+    [SerializeField] private Animator animatorAmigo;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Spawn()
     {
-        
+        isAmigoHappy = false;
     }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.name == "playerReality")
+        {
+            StartCoroutine(WaitForAmigo());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D coll)
+    {
+        if (isAmigoHappy)
+        {
+            GameManager.Instance.currentHealth++;
+            
+            print("amigo feliz :)");
+        }
+        else
+        {
+            print("noooo amigo triste");
+            animatorAmigo.SetTrigger("IsSad");
+        }
+    }
+
+    private IEnumerator WaitForAmigo()
+    {
+        yield return new WaitForSeconds(Random.Range(3, 5));
+        animatorAmigo.SetTrigger("IsHappy");
+        isAmigoHappy = true;
+    }
+
 }
