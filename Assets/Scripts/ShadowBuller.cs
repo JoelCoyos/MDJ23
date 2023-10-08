d4ad25f854c591ae49bdf8d70f60b91579cb94de
CreepiestWeirdo <federicopoliseno@yahoo.com.ar>
2023-10-08 12:02:09 -0300

    ShadowScene

---
+++ Assets/Scripts/ShadowBuller.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class ShadowBuller : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 3.0f;
+    public GameObject bullyingPrefab;
+    public float shootDistance = 5.0f;
+    public float timeBetweenShots = 2.0f;
+    private float lastShotTime;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = (player.position - transform.position).normalized;
+        transform.Translate(moveDirection *  moveSpeed * Time.deltaTime);
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (moveDirection.magnitude <= shootDistance) 
+        { 
+            if (Time.time -  lastShotTime >= timeBetweenShots) 
+            {
+                Shoot();
+            }
+        }
+
+    }
+    void Shoot()
+    {
+        lastShotTime = Time.time;
+
+        GameObject newProjectile = Instantiate(bullyingPrefab, transform.position, Quaternion.identity);
+
+        Vector3 moveDirection = (player.position - transform.position).normalized;
+
+        Rigidbody2D rb = newProjectile.GetComponent<Rigidbody2D>();
+        rb.velocity = moveDirection * moveSpeed;
+    }
+}
